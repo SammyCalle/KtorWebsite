deee7d23638638a48e4ac30400bfc370e3f72bd0
Sammy Calle <sammycallet@gmail.com>
2025-03-06 23:34:10 +0200

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package sammy.ktor.tutorials
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureTemplating()
+    configureRouting()
+}
